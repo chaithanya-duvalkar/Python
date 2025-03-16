1e02c3befd56162ca10ac4d94a830a78593180ad
Chaithanya S <chaithanya9ig@gmail.com>
2025-03-16 21:00:59 +0530

    initial commit

---
+++ pgm2.py
@@ -0,0 +1,59 @@
+'''
+read and print first line
+a=input()
+print(a)
+'''
+
+'''read and print first line and second line as #'''
+a=input()
+print(a)
+print("###")
+
+
+'''read two lines and reverse order'''
+a=input()
+b=input()
+print(a)
+print(b)
+
+
+'''read two lines and print first line'''
+a=input()
+b=input()
+print(b)
+
+
+'''read two lines and reverse order'''
+a=input()
+b=input()
+print(b)
+print(a)
+
+
+'''reads the name and age of a person and prints them in the given format'''
+'''_name_ is 21 years old'''
+name=input()
+age=input()
+print(name + " is " + age + " years old")
+
+
+'''to enter full_name'''
+first_name=input()
+last_name=input()
+print(first_name+" "+last_name)
+
+
+'''read two words and print the resultant word by joining the two words'''
+a=input()
+b=input()
+print(a+b)
+
+
+'''read word and print the line after the msg "Given input:'''
+a=input()
+print("Given input: "+a)
+
+
+'''read a word W as input and prints "Hello" followed by the given word W'''
+a=input()
+print("Hello "+a)
