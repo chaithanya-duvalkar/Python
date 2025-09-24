40fc09ce7cb0b54f4f7acbe05b072a4c9f4d9028
Chaithanya S <chaithanya9ig@gmail.com>
2025-09-24 12:49:10 +0530

    made some changes in the folders

---
+++ Learning/app.py
@@ -0,0 +1,38 @@
+x = 1
+y = 2
+course = "Python Programming"
+print(course)
+course = "Python \"Programming"
+print(course)
+course = "Python \'Programming"
+print(course)
+course = "Python \\Programming"
+print(course)
+course = "Python \nProgramming"
+print(course)
+Ravi
+----
+------
+Falls
+True
+False
+9035793924
+903 5793924
+9035793924
+90357--93924
+chai
+the cat and the dog
+True
+False
+True
+True
+False
+CHAITHEJ
+chaithej
+p%u&r*p%l&e.
+B2r2e2a2d@
+%%%%%%%
+1000g, 100g, 250g, 500g
+True
+ef
+False
---
+++ Learning/demo.py
@@ -0,0 +1,13 @@
+x = 9
+x = 1
+y = 2
+course = "Python Programming"
+print(course)
+course = "Python \"Programming"
+print(course)
+course = "Python \'Programming"
+print(course)
+course = "Python \\Programming"
+print(course)
+course = "Python \nProgramming"
+print(course)
