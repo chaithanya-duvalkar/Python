925e664160e676f510d945e3b828cd59dd6f4724
Chaithanya S <chaithanya9ig@gmail.com>
2025-10-12 22:14:34 +0530

    python  pgm added

--- Learning/demo.py
+++ Learning/demo.py
@@ -1,78 +1,92 @@
-# # for x in "Chaithanya":
-# #     print(x)
+# # # for x in "Chaithanya":
+# # #     print(x)
 
-# # for i in [1, 2, 3, 4]:
-# #     print(i)
+# # # for i in [1, 2, 3, 4]:
+# # #     print(i)
 
-# # command = ""
-# # # while command != "quit" and command != "QUIT":
-# # while command.lower() != "quit":
-# #     command = input(">")
-# #     print("ECHO", command)
+# # # command = ""
+# # # # while command != "quit" and command != "QUIT":
+# # # while command.lower() != "quit":
+# # #     command = input(">")
+# # #     print("ECHO", command)
 
-# # num1 = 1
-# # num2 = 10
-# # count = 0
-# # # for i in range(num1, num2):
-# # for i in range(1, 10):
-# #     if (i % 2) == 0:
-# #         print(i)
-# #         count += 1
-# # print("total even numbers between 1 and 10 is", count)
+# # # num1 = 1
+# # # num2 = 10
+# # # count = 0
+# # # # for i in range(num1, num2):
+# # # for i in range(1, 10):
+# # #     if (i % 2) == 0:
+# # #         print(i)
+# # #         count += 1
+# # # print("total even numbers between 1 and 10 is", count)
 
-# def greet():
-#     print("hi Chai..!")
-
-
-# greet()
+# # def greet():
+# #     print("hi Chai..!")
 
 
-# def greet_argument(fname, lname):
-#     print(f"Hi {fname} {lname}")
-#     print("Welcome..!")
+# # greet()
 
 
-# greet_argument("Chai", "Duvalkar")
+# # def greet_argument(fname, lname):
+# #     print(f"Hi {fname} {lname}")
+# #     print("Welcome..!")
 
 
-# def increment(num, another, by=1):
-#     return num+another+by
+# # greet_argument("Chai", "Duvalkar")
 
 
-# print(increment(8, 1))
+# # def increment(num, another, by=1):
+# #     return num+another+by
 
 
-# def multiply(x, y):
-#     return x*y
+# # print(increment(8, 1))
 
 
-# print(multiply(3, 3))
+# # def multiply(x, y):
+# #     return x*y
 
 
-# def multiply(*numbers):
-#     total = 1
-#     for i in numbers:
-#         total *= i
-#     return total
+# # print(multiply(3, 3))
 
 
-# print(multiply(2, 3, 4, 5))
-
-def print_reverse(n):
-    if n == 0:
-        return
-    print(n)
-    print_reverse(n-1)
+# # def multiply(*numbers):
+# #     total = 1
+# #     for i in numbers:
+# #         total *= i
+# #     return total
 
 
-print_reverse(10)
+# # print(multiply(2, 3, 4, 5))
+
+# def print_reverse(n):
+#     if n == 0:
+#         return
+#     print(n)
+#     print_reverse(n-1)
 
 
-def printNumbers(n):
-    if n == 0:
-        return
-    printNumbers(n-1)
-    print(n)
+# print_reverse(10)
 
 
-printNumbers(10)
+# def printNumbers(n):
+#     if n == 0:
+#         return
+#     printNumbers(n-1)
+#     print(n)
+
+
+# printNumbers(10)
+
+
+color="*VIOLET*"
+new_word=color[::3]
+print(new_word)
+
+word1="111"
+word2="!!!"
+print(word1==word1.lower())
+print(word2==word2.lower())
+
+word="aabbaabbaa "
+word=word.strip("a")
+print(len(word))
