d0f6e2ebd1d5999ff46944966d305de880acb2ad
Chaithanya S <chaithanya9ig@gmail.com>
2025-11-10 00:07:57 +0530

    oops pgm added

---
+++ Learning/oops.py
@@ -0,0 +1,24 @@
+class car:
+    color="yellow"
+    brand="tata"
+
+car1=car()
+print(car1.color)
+print(car1.brand)
+ 
+class student:
+
+    #default constructor
+    def __init__(self):
+        pass
+
+    #parameter constructor
+    def __init__(self,fname):
+        self.name=fname
+        print("adding new student into DB")
+        
+s1=student("chai")
+print(s1.name)
+
+s2=student("chukki")
+print(s2.name)
